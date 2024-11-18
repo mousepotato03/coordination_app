e02e4d7836701173c47fb370145691b2a7e4a631
mousepotato03 <seongjin2@kau.kr>
2024-11-18 19:48:44 +0900

    Closet Page TabBar + View

---
+++ lib/presentation/pages/closet/closet_category.dart
@@ -0,0 +1,9 @@
+enum ClosetCategory {
+  top('상의'),
+  bottom('하의'),
+  outer('아우터'),
+  shoes('신발');
+
+  final String label;
+  const ClosetCategory(this.label);
+}
--- lib/presentation/pages/closet/closet_page.dart
+++ lib/presentation/pages/closet/closet_page.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/theme/constant/app_colors.dart';
+import 'closet_category.dart';
+
 class ClosetPage extends StatelessWidget {
   const ClosetPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child:Text("ClosetPage"));
+    return DefaultTabController(
+      length: ClosetCategory.values.length,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorColor: AppColors.primary,
+            labelColor: AppColors.black,
+            unselectedLabelColor: AppColors.black,
+            tabs: ClosetCategory.values
+                .map((category) => Tab(text: category.label))
+                .toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: ClosetCategory.values.map((category) {
+            return Center(
+              child: Text('${category.label} 콘텐츠'),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
