a90a316d4bd7184c68438976d6feacc4bf0365fd
mousepotato03 <seongjin2@kau.kr>
2025-01-10 00:41:33 +0900

    TextField 입력 시 overpixel 오류 수정

--- lib/presentation/pages/profile/profile_page.dart
+++ lib/presentation/pages/profile/profile_page.dart
@@ -31,51 +31,63 @@ class _ProfilePageState extends ConsumerState<ProfilePage> {
     final status = ref.watch(mySizeInfoProvider).status;
 
     return Scaffold(
-        body: Center(
-      child: switch (status) {
-        Status.initial || Status.loading => const CircularProgressIndicator(),
-        Status.success => Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const NotePadWidget(
-                children: [
-                  BaseInfoPage(),
-                  UpperBodyInfoPage(),
-                  LowerBodyInfoPage(),
-                  ShoesInfoPage(),
-                ],
-              ),
-              height30,
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primary,
+      body: Center(
+        child: switch (status) {
+          Status.initial || Status.loading => const CircularProgressIndicator(),
+          Status.success => SingleChildScrollView(
+              physics: const NeverScrollableScrollPhysics(),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom + 16,
                 ),
-                onPressed: () {
-                  if (isEditMode) {
-                    FocusScope.of(context).unfocus();
-                    WidgetsBinding.instance.addPostFrameCallback((_) async {
-                      await ref
-                          .read(mySizeInfoProvider.notifier)
-                          .saveMySizeInfo();
-                      ref.read(avatarStateProvider.notifier).getBodyInfo();
-                    });
-                  }
-                  ref.read(mySizeInfoProvider.notifier).toggleEditMode();
-                },
-                child: SizedBox(
-                  width: 100,
-                  height: 35,
-                  child: Icon(
-                    isEditMode ? Icons.check : Icons.edit,
-                    color: Colors.white,
-                    size: 30,
-                  ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const NotePadWidget(
+                      children: [
+                        BaseInfoPage(),
+                        UpperBodyInfoPage(),
+                        LowerBodyInfoPage(),
+                        ShoesInfoPage(),
+                      ],
+                    ),
+                    height30,
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.primary,
+                      ),
+                      onPressed: () {
+                        if (isEditMode) {
+                          FocusScope.of(context).unfocus();
+                          WidgetsBinding.instance
+                              .addPostFrameCallback((_) async {
+                            await ref
+                                .read(mySizeInfoProvider.notifier)
+                                .saveMySizeInfo();
+                            ref
+                                .read(avatarStateProvider.notifier)
+                                .getBodyInfo();
+                          });
+                        }
+                        ref.read(mySizeInfoProvider.notifier).toggleEditMode();
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 35,
+                        child: Icon(
+                          isEditMode ? Icons.check : Icons.edit,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        Status.error => const Text("에러"),
-      },
-    ));
+            ),
+          Status.error => const Text("에러"),
+        },
+      ),
+    );
   }
 }
