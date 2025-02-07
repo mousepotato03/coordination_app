d9bff57653b2120607def299d520147debecf909
mousepotato03 <seongjin2@kau.kr>
2025-02-07 23:11:42 +0900

    Ignore key file

--- lib/core/secure_key.dart
+++
@@ -1,8 +0,0 @@
-const gptKey =
-    "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";
-
-const gptAssistantsKey = "asst_SyvkvjBkioLiLsA1EZp0yPtC";
-
-const kakaoNativeAppKey = "d9e3f877b7a01513af74476da9685d93";
-
-const localServerAddress = "http://192.168.219.102:8000";
